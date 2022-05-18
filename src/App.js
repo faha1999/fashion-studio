a53f1371df6150a218416a61aa0b0c27824dc833
faha1999 <kafaha1999@gmail.com>
2022-05-18 17:22:51 +0600

    added v

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
 
 function App() {
-  return <div className="App">hello</div>;
+  const containerRef = useRef(null);
+
+  return (
+    <LocomotiveScrollProvider
+      options={{
+        smooth: true,
+      }}
+      watch={[]}
+      containerRef={containerRef}
+    >
+      <main data-scroll-container ref={containerRef}>
+        hello
+      </main>
+    </LocomotiveScrollProvider>
+  );
 }
 
 export default App;
