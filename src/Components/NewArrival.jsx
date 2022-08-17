ce5564f43141861342cbd2e0e314c17d73ebd77e
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-08-17 16:44:08 +0600

    added gsap[ bit has issue

--- src/Components/NewArrival.jsx
+++ src/Components/NewArrival.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 import img1 from '../assets/Images/11.webp';
 import img2 from '../assets/Images/12.webp';
@@ -15,8 +17,65 @@ const Product = ({ img, title = '' }) => {
 };
 
 export const NewArrival = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  const ref = useRef(null);
+  const ScrollingRef = useRef(null);
+
+  useLayoutEffect(() => {
+    let element = ref.current;
+    let scrollingElement = ScrollingRef.current;
+
+    let t1 = gsap.timeline();
+
+    setTimeout(() => {
+      t1.to(element, {
+        scrollTrigger: {
+          trigger: element,
+          start: 'top top',
+          end: 'bottom+=100% top-=100%',
+          scroller: '.App', // locomotive element
+          scrub: true,
+          pin: true,
+          //   markers:true,
+        },
+        // we have to increase scrolling height of this section same as the scrolling element width
+        ease: 'none,',
+      });
+
+      // Verticle Scrolling
+      t1.fromTo(
+        scrollingElement,
+        {
+          y: '0',
+        },
+
+        {
+          y: '-100%',
+          scrollTrigger: {
+            trigger: scrollingElement,
+            start: 'top top',
+            end: 'bottom top',
+            scroller: '.App', // locomotive element
+            scrub: true,
+
+            // markers: true,
+          },
+          // we have to increase scrolling height of this section same as the scrolling element width
+        },
+      );
+      ScrollTrigger.refresh();
+    }, 1000);
+
+    return () => {
+      // Let's clear instances
+      //t1.kill();
+      //ScrollTrigger.kill();
+    };
+  }, []);
+
   return (
-    <section className="NewArrivalSection">
+    <section className="NewArrivalSection" ref={ref} id="new-arrival">
       <div className="Overlay"></div>
       <h1
         className="Title"
@@ -27,7 +86,7 @@ export const NewArrival = () => {
         New Arrivals
       </h1>
 
-      <div className="Container">
+      <div className="Container" ref={ScrollingRef}>
         <Product img={img1} title="Denim" />
         <Product img={img2} title="Cool Dresses" />
         <Product img={img3} title="Jackets" />
