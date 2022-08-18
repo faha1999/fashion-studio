e6b7e41b000df1dff005a580a55b095cff8c43d1
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-08-18 22:42:24 +0600

    complete footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 import Logo from '../assets/Svgs/star_white_48dp.svg';
+import { useLocomotiveScroll } from 'react-locomotive-scroll';
 
 export const Footer = () => {
+  const { scroll } = useLocomotiveScroll();
+
+  const handleScroll = (id) => {
+    let elem = document.querySelector(id);
+
+    scroll.scrollTo(elem, {
+      offset: '-100',
+      duration: '2000',
+      easing: [0.25, 0.0, 0.35, 1.0],
+    });
+  };
+
   return (
     <section className="FooterContainer">
       <div className="LogoContainer">
@@ -12,12 +26,19 @@ export const Footer = () => {
         </h3>
       </div>
 
-      <footer>
+      <motion.footer
+        initial={{ y: '-400px' }}
+        whileInView={{ y: 0 }}
+        viewport={{ once: false }}
+        transition={{
+          duration: 1.5,
+        }}
+      >
         <ul>
-          <li>home</li>
-          <li>about</li>
-          <li>shop</li>
-          <li>new arrival</li>
+          <li onClick={() => handleScroll('#home')}>home</li>
+          <li onClick={() => handleScroll('.about')}>about</li>
+          <li onClick={() => handleScroll('#shop')}>shop</li>
+          <li onClick={() => handleScroll('#new-arrival')}>new arrival</li>
           <li>
             <a href="https://google.com" target="_blank" rel="noreferrer">
               look book
@@ -31,10 +52,19 @@ export const Footer = () => {
         </ul>
 
         <div className="Bottom">
-          <span>
-            {' '}
-            &copy; {new Date().getFullYear()}. All Rights Reserved. Made with
-            &hearts; by &nbsp;{' '}
+          <span
+            data-scroll
+            data-scroll-speed="2"
+            data-scroll-direction="horizontal"
+          >
+            &copy; {new Date().getFullYear()}. All Rights Reserved.
+          </span>
+          <span
+            data-scroll
+            data-scroll-speed="-2"
+            data-scroll-direction="horizontal"
+          >
+            Made with &hearts; by &nbsp;{' '}
             <a
               href="https://github.com/faha1999"
               target="_blank"
@@ -44,7 +74,7 @@ export const Footer = () => {
             </a>
           </span>
         </div>
-      </footer>
+      </motion.footer>
     </section>
   );
 };
